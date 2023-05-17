92c885e82e985cf0c54776e45473168d2a943fe2
Matias Pulkkinen <matias.pulkkinen@hotmail.com>
2023-05-17 13:14:00 +0300

    Korjattu saman sanan uudelleen lisäys

--- sanat.py
+++ sanat.py
@@ -14,16 +14,17 @@ class Sanalista():
     
     def lisaa_sana():
         try:
-            tiedosto = open("words.txt", "a")
+            tiedosto = open("words.txt", "r+")
         except FileNotFoundError:
             print("Tiedostoa ei löytynyt..")
             SystemExit 
+        lista = []
+        for rivi in tiedosto:
+            lista.append(rivi.strip())
         sana = input("Syötä sana: ")
-        tiedosto.write("\n"+sana.lower())
+        if sana in lista:
+            print("Sana on jo listassa")
+            return
+        tiedosto.write("\n" + sana.lower())
         tiedosto.close()
         print("Sana lisätty!")
-    
-
-
-
-        
