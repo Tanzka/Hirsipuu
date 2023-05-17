70fb4fe793eaa35caf9c837e9917a7ea75292556
Matias Pulkkinen <matias.pulkkinen@hotmail.com>
2023-05-17 11:41:07 +0300

    sanan arvaus toimii

--- hangman.py
+++ hangman.py
@@ -1,1 +1,51 @@
-#Hangman
+class Hirsipuu:
+    def __init__(self, sana: str):
+        self.__sana = sana
+        self.__arvauksia = 5
+
+    # korvaa arvaamattomat kirjaimet viivoilla
+    def sana_piilotus(self, sana: str, arvatut: list):
+        piilotettu = ""
+        for kirjain in sana:
+            if kirjain in arvatut:
+                piilotettu += kirjain
+            else:
+                piilotettu += "_"
+        return piilotettu
+    
+    # arvaa kirjaimia kunnes sana on arvattu tai arvaukset loppuvat
+    def pelaa(self):
+        kirjaimet = "abcdefghijklmnopqrstuvwxyzåäö"
+        arvatut = []
+
+        print("Syötä kirjaimia kunnes sana on arvattu, 0 lopettaa")
+        while True:
+            print(f"Arvauksia jäljellä: {self.__arvauksia}")
+            print(self.sana_piilotus(self.__sana, arvatut))
+            print("")
+            arvaus = input("Syötä kirjain: ").lower()
+            if arvaus == "0":
+                break
+            if len(arvaus) != 1 or arvaus not in kirjaimet:   # jos ei ole kirjain, arvaa uudestaan
+                print("Ei ole kirjain")
+                continue
+            if arvaus in arvatut:   # jos kirjain on jo arvattu, arvaa uudestaan
+                print("Arvattu jo")
+                continue
+            arvatut.append(arvaus)   # lisätään kirjain jo arvattuihin
+            if arvaus in self.__sana:   # löytyykö kirjain arvattavasta sanasta
+                print("Löytyy!")
+                if self.sana_piilotus(self.__sana, arvatut) == self.__sana:
+                    print(":)")   # voitto
+                    break
+            else:
+                print("Ei löydy")
+                self.__arvauksia -= 1
+                if self.__arvauksia == 0:
+                    print(":(")   # häviö
+                    break
+
+
+"""# testausta
+hp = Hirsipuu("sana")
+hp.pelaa()"""
